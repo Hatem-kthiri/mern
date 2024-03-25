d22651eded4accb7ac87aeba3d3e400bb9a485c0
Hatem-kthiri <hatemkthiri1997@gmail.com>
2024-03-25 14:55:54 +0100

    initialisation projet mern

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const user = require("./routes/user");
 const connect = require("./config/connectDB");
-app.use("/api/v1/auth", user);
 const app = express();
+app.use("/api/v1/auth", user);
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
